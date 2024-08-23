6ab6164aaa5e5731b503f5d351a00ec1bd111e07
Sofiane5900 <132440690+Sofiane5900@users.noreply.github.com>
2024-08-23 17:26:58 +0200

    ajout du dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
-    <>
-    Hello World
-    </>
+    <div className="m-4">
+      <Button>Click me!</Button>
+      <Input></Input>
+    
+    </div>
   );
 }
